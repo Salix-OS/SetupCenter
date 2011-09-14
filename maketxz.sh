afbeb53b8ec941ed3afa09590555bd52d6b2e736
Cyrille Pontvieux <jrdATenialisDOTnet>
2011-09-14 10:02:14 +0000

    maketxz.sh changed to use a RLZ variable. new version in the trunk for persistence-wizard, with updated translations. gparted no more on the desktop on salix-live. new getpkgs.sh script that can be used to only upgrade packages.

--- maketxz.sh
+++ maketxz.sh
@@ -6,6 +6,7 @@ mkdir -p pkg
 export DESTDIR=$PWD/pkg
 ./install.sh
 VER=$(grep 'version =' src/setupcenter.py | head -n 1 | sed "s/.*'\(.*\)'/\1/" | tr -d '-')
+RLZ=1plb
 cd pkg
 cat <<EOF > install/slack-desc
 setupcenter: SetupCenter - A centralized setup panel.
@@ -20,8 +21,8 @@ setupcenter:
 setupcenter:
 setupcenter:
 EOF
-makepkg -l y -c n ../setupcenter-$VER-noarch-1plb.txz
+makepkg -l y -c n ../setupcenter-$VER-noarch-$RLZ.txz
 cd ..
-echo -e "python,pygtk,pyxdg" > setupcenter-$VER-noarch-1plb.dep
-md5sum setupcenter-$VER-noarch-1plb.txz > setupcenter-$VER-noarch-1plb.md5
+echo -e "python,pygtk,pyxdg" > setupcenter-$VER-noarch-$RLZ.dep
+md5sum setupcenter-$VER-noarch-$RLZ.txz > setupcenter-$VER-noarch-$RLZ.md5
 rm -rf pkg
